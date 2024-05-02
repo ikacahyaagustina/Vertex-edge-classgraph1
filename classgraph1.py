cadae3ad3869983895648a018fe7173b152758ba
savinkaa <149308552+savinkaa@users.noreply.github.com>
2024-05-02 12:02:28 +0700

    Update classgraph1.py

--- classgraph1.py
+++ classgraph1.py
@@ -18,3 +18,40 @@ class Peta:
     def printpeta(self):
         for kota in self.adj_list:
             print(kota, ":", self.adj_list[kota]) 
+
+#contoh penggunaan:
+if __name__ == "__main__":
+    graph = Peta()
+
+    #tambah vertex (nama kota)
+    graph.addkota('Surabaya')
+    graph.addkota('Semarang')
+    graph.addkota('Jakarta')
+    graph.addkota('Lampung')
+    graph.addkota('Jambi')
+    graph.addkota('Riau')
+    graph.addkota('Kepulauan Riau')
+    graph.addkota('Medan')
+    graph.addkota('Kalimantan Barat')
+    graph.addkota('Kalimantan Timur')
+    graph.addkota('Sulawesi Utara')
+    graph.addkota('Maluku')
+    graph.addkota('Makassar')
+    graph.addkota('Papua Barat')
+    graph.addkota('Nusa Tenggara Timur')
+    graph.addkota('Bali')
+
+    #tambah edge (garis hubungan antar kota)
+    graph.addjalan('Surabya', 'Semarang')
+    graph.addjalan('Surabaya', 'Bali')
+    graph.addjalan('Semarang', 'Jakarta')
+    graph.addjalan('Semarang', 'Surabaya')
+    graph.addjalan('Jakarta', 'Lampung')
+    graph.addjalan('Jakarta', 'Kalimantan Timur')
+    graph.addjalan('Lampung', 'Jambi')
+    graph.addjalan('Jambi', 'Riau')
+    graph.addjalan('Riau', 'Kepulauan Riau')
+    graph.addjalan('Kepulauan Riau', 'Medan')
+    graph.addjalan('Kepulauan Riau', 'Sulawesi Utara')
+    graph.addjalan('Medan', 'Kalimantan Barat')
+    graph.addjalan('Medan', 'Lampung')
